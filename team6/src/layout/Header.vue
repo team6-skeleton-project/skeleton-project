def10ec5aa945e5d1f6d68f8dcf8794fcc5e333b
home <home@homeui-MacBookAir.local>
2026-04-08 14:55:46 +0900

    [feat#1]헤더생성

--- team6/src/layout/Header.vue
+++ team6/src/layout/Header.vue
@@ -1,2 +1,22 @@
-<template></template>
-<script></script>
+<template>
+  <header class="header">
+    <h1>가계부</h1>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  max-width: 390px;
+  height: 56px;
+
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+
+  background: #fff;
+  border-bottom: 1px solid #eee;
+}
+</style>
